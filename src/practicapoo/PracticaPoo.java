2c094d5b17eee0572d1cd2dd15131bb8d1172924
balalofernandez <balalo.fernandez@gmail.com>
2020-06-23 16:31:32 +0200

    first commit

--- src/practicapoo/PracticaPoo.java
+++ src/practicapoo/PracticaPoo.java
@@ -15,7 +15,9 @@ public class PracticaPoo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String a;
+        a = "5";
+        System.out.println(a);
     }
     
 }
