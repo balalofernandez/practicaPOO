9fcb9064e3cf4b076d609e501e73706b06d26d50
balalofernandez <balalo.fernandez@gmail.com>
2020-06-23 17:48:41 +0200

    cambios en el rey

---
+++ src/Piezas/Rey.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Piezas;
+
+/**
+ *
+ * @author balalo
+ */
+public class Rey extends Pieza{
+    private static final char SIMBOLO ='R';
+    
+    public Rey(Color color){
+        super(SIMBOLO, color);
+    }
+    
+    
+}
