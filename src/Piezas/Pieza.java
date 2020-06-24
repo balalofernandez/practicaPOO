6a849b760cb1ecebfcfc761774c7887b4d0b9992
balalofernandez <balalo.fernandez@gmail.com>
2020-06-24 10:34:41 +0200

    Añadimos las piezas

--- src/Piezas/Pieza.java
+++ src/Piezas/Pieza.java
@@ -18,4 +18,5 @@ public abstract class Pieza {
         this.color = color;
     }
     
+    
 }
--- src/Piezas/Rey.java
+++ src/Piezas/Rey.java
@@ -16,5 +16,4 @@ public class Rey extends Pieza{
         super(SIMBOLO, color);
     }
     
-    
 }
